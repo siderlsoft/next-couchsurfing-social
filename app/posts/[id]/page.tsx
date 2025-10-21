02aa58ee3b940fca70bd574a26b4381c3317a6e5
siderlsoft <sahid.hm.21@gmail.com>
2025-10-20 23:47:07 -0600

    add post detail component

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,20 @@
+import PostCard from "@/app/(components)/PostCard";
+import { getPost } from '@/lib/api';
+import Link from "next/link";
+
+export default async function PostDetail({ params }: { params: { id: string } }) {
+    const post = await getPost(params.id);
+
+    return (
+        <section className="flex flex-col items-center mt-4 space-y-4">
+            <Link
+                href="/"
+                className="self-start text-brand-600 hover:underline hover:text-brand-700 transition-colors ml-4 mb-2"
+            >
+                ← Back to feed
+            </Link>
+
+            <PostCard post={post} />
+        </section>
+    );
+}
